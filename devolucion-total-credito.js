c7d2bb639466e56bc8fbbe4e5e389a7f31b13988
AndyOtarola <otarolaandy@gmail.com>
2024-03-20 09:29:16 -0500

    Create devolucion-total-credito.js

---
+++ devolucion-total-credito.js
@@ -0,0 +1,140 @@
+{
+  "typeCatalogSunatId": 6,
+  "transactionId": [],
+  "transactions": [],
+  "totalTaxesAmount": {
+    "subtotalWithoutTax": 5.42,
+    "iva": 0,
+    "ice": 0,
+    "irbpnr": 0,
+    "tip": 0
+  },
+  "detailsPayments": [
+    {
+      "amount": -6.4,
+      "expirationDate": "2024-03-20"
+    }
+  ],
+  "taxesAmount": {
+    "recorded": 5.42,
+    "inactive": 0,
+    "exonerated": 0,
+    "free": 0,
+    "export": 0,
+    "icbper": 0
+  },
+  "taxes": 0.98,
+  "subtotal": 5.42,
+  "salDocumentsId": 14864343,
+  "paymentMethodId": 2,
+  "flagTransfer": true,
+  "exchangeRate": 3.6,
+  "discount": 0,
+  "details": [
+    {
+      "discount": 0,
+      "brandSpId": 39376,
+      "brandName": "SIN MARCA",
+      "groupType": 3,
+      "categoryName": "LIMPIEZA",
+      "categoryId": 60763,
+      "description": "Limpia todo sapolio floral 900 ml",
+      "productCode": "7751851000758",
+      "productCubsoCode": null,
+      "unitCode": "UND",
+      "unitName": "UNIDAD",
+      "additionalInformation": {
+        "descriptionProducts": "Limpia todo sapolio floral 900 ml",
+        "autoBarcode": "7",
+        "salPriceListId": 3356
+      },
+      "codeTaxes": "01",
+      "discountPercentage": 0,
+      "flagFree": 0,
+      "warehouseName": "TIENDA 1",
+      "brandId": 38855,
+      "quantity": 1,
+      "price": 3.9,
+      "productType": 1,
+      "salePrice": 3.9,
+      "stockQuantity": 289,
+      "subtotalWithoutTax": 3.31,
+      "tax": 18,
+      "taxAmount": 0.5949,
+      "priceCost": 0.001,
+      "unitConversion": 1,
+      "unitId": 11264,
+      "unitPrice": 3.3051,
+      "unitQuantity": 1,
+      "warProductsId": 3784376,
+      "warWarehousesId": 6194,
+      "taxes": [
+        {
+          "amount": 0.5949,
+          "code": "01",
+          "codePercentage": "01",
+          "percentage": 18,
+          "subtotal": 3.9,
+          "taxSchemeName": "IGV IMPUESTO GENERAL A LAS VENTAS"
+        }
+      ]
+    },
+    {
+      "discount": 0,
+      "brandSpId": 39376,
+      "brandName": "SIN MARCA",
+      "groupType": 3,
+      "categoryName": "S/CATEGORIA",
+      "categoryId": 60765,
+      "description": "San luis manzana ",
+      "productCode": "7750182007757",
+      "productCubsoCode": null,
+      "unitCode": "SIN",
+      "unitName": "SIN_UNIDAD",
+      "additionalInformation": {
+        "descriptionProducts": "San luis manzana ",
+        "autoBarcode": "9",
+        "salPriceListId": 3356
+      },
+      "codeTaxes": "01",
+      "discountPercentage": 0,
+      "flagFree": 0,
+      "warehouseName": "TIENDA 1",
+      "brandId": 38855,
+      "quantity": 1,
+      "price": 2.5,
+      "productType": 1,
+      "salePrice": 2.5,
+      "stockQuantity": 73,
+      "subtotalWithoutTax": 2.12,
+      "tax": 18,
+      "taxAmount": 0.3813,
+      "priceCost": 0.001,
+      "unitConversion": 1,
+      "unitId": 12282,
+      "unitPrice": 2.1186,
+      "unitQuantity": 1,
+      "warProductsId": 3784380,
+      "warWarehousesId": 6194,
+      "taxes": [
+        {
+          "amount": 0.3813,
+          "code": "01",
+          "codePercentage": "01",
+          "percentage": 18,
+          "subtotal": 2.5,
+          "taxSchemeName": "IGV IMPUESTO GENERAL A LAS VENTAS"
+        }
+      ]
+    }
+  ],
+  "currency": "PEN",
+  "commentary": "",
+  "amountCredit": 0,
+  "amountCash": 6.4,
+  "amount": 6.4,
+  "tip": 0,
+  "dateOnline": "1710944840503",
+  "warehouseCodeTaxes": null,
+  "warehouseName": "TIENDA 1"
+}
