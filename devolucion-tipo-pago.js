7894b0c68e9661db8186ea0405595ff018c3b8b7
AndyOtarola <otarolaandy@gmail.com>
2024-03-20 14:20:40 -0500

    Create devolucion-tipo-pago.js

---
+++ devolucion-tipo-pago.js
@@ -0,0 +1,222 @@
+{
+  "typeCatalogSunatId": 6,
+  "transactionId": [
+    1,
+    6,
+    38,
+    65,
+    63,
+    66,
+    67,
+    68
+  ],
+  "transactions": [
+    {
+      "amount": -1,
+      "currency": "PEN",
+      "paymentAmount": -1,
+      "paymentDate": "2024-03-20",
+      "typePaymentId": 1
+    },
+    {
+      "amount": -1,
+      "currency": "PEN",
+      "paymentAmount": -1,
+      "paymentDate": "2024-03-20",
+      "typePaymentId": 6,
+      "additionalInformation": {
+        "dateReference": "2024-03-20"
+      },
+      "bankAccountId": 1510,
+      "urlImage": null,
+      "documentNumber": "Deposito"
+    },
+    {
+      "amount": -1,
+      "currency": "PEN",
+      "paymentAmount": -1,
+      "paymentDate": "2024-03-20",
+      "typePaymentId": 38
+    },
+    {
+      "amount": -1,
+      "currency": "PEN",
+      "paymentAmount": -1,
+      "paymentDate": "2024-03-20",
+      "typePaymentId": 65,
+      "documentNumber": "Niubiz - voucher"
+    },
+    {
+      "amount": -1,
+      "currency": "PEN",
+      "paymentAmount": -1,
+      "paymentDate": "2024-03-20",
+      "typePaymentId": 63,
+      "additionalInformation": {
+        "dateReference": "2024-03-20"
+      },
+      "bankAccountId": 1510,
+      "urlImage": null,
+      "documentNumber": null
+    },
+    {
+      "amount": -1,
+      "currency": "PEN",
+      "paymentAmount": -1,
+      "paymentDate": "2024-03-20",
+      "typePaymentId": 66,
+      "documentNumber": "izipay- voucher"
+    },
+    {
+      "amount": -1,
+      "currency": "PEN",
+      "paymentAmount": -1,
+      "paymentDate": "2024-03-20",
+      "typePaymentId": 67,
+      "documentNumber": "vendemas-voucher"
+    },
+    {
+      "amount": -7,
+      "currency": "PEN",
+      "paymentAmount": -7,
+      "paymentDate": "2024-03-20",
+      "typePaymentId": 68,
+      "additionalInformation": {
+        "dateReference": "2024-03-20"
+      },
+      "bankAccountId": 1510,
+      "urlImage": null,
+      "documentNumber": null
+    }
+  ],
+  "totalTaxesAmount": {
+    "subtotalWithoutTax": 11.86,
+    "iva": 0,
+    "ice": 0,
+    "irbpnr": 0,
+    "tip": 0
+  },
+  "detailsPayments": [],
+  "taxesAmount": {
+    "recorded": 11.86,
+    "inactive": 0,
+    "exonerated": 0,
+    "free": 0,
+    "export": 0,
+    "icbper": 0
+  },
+  "taxes": 2.14,
+  "subtotal": 11.86,
+  "salDocumentsId": 14871651,
+  "paymentMethodId": 1,
+  "flagTransfer": true,
+  "exchangeRate": 3.6,
+  "discount": 0,
+  "details": [
+    {
+      "discount": 0,
+      "brandSpId": 47036,
+      "brandName": "PURA VIDA",
+      "groupType": 3,
+      "categoryName": "AGUA Y HIELO",
+      "categoryId": 55594,
+      "description": "Agua pura vida sin gas 500 ml",
+      "productCode": "7751271033619",
+      "productCubsoCode": null,
+      "unitCode": "SIN",
+      "unitName": "SIN_UNIDAD",
+      "additionalInformation": {
+        "descriptionProducts": "Agua pura vida sin gas 500 ml",
+        "autoBarcode": "24",
+        "salPriceListId": 3356
+      },
+      "codeTaxes": "01",
+      "discountPercentage": 0,
+      "flagFree": 0,
+      "warehouseName": "TIENDA 1",
+      "brandId": 47036,
+      "quantity": 2,
+      "price": 2,
+      "productType": 1,
+      "salePrice": 2,
+      "stockQuantity": 276,
+      "subtotalWithoutTax": 3.39,
+      "tax": 18,
+      "taxAmount": 0.6102,
+      "priceCost": 1.0167,
+      "unitConversion": 1,
+      "unitId": 12282,
+      "unitPrice": 1.6949,
+      "unitQuantity": 2,
+      "warProductsId": 3862975,
+      "warWarehousesId": 6194,
+      "taxes": [
+        {
+          "amount": 0.6102,
+          "code": "01",
+          "codePercentage": "01",
+          "percentage": 18,
+          "subtotal": 4,
+          "taxSchemeName": "IGV IMPUESTO GENERAL A LAS VENTAS"
+        }
+      ]
+    },
+    {
+      "discount": 0,
+      "brandSpId": 52669,
+      "brandName": "MOTOROLA",
+      "groupType": 3,
+      "categoryName": "AGUA Y HIELO",
+      "categoryId": 55594,
+      "description": "Tienda 2",
+      "productCode": "000999",
+      "productCubsoCode": null,
+      "unitCode": "UND",
+      "unitName": "UNIDAD",
+      "additionalInformation": {
+        "descriptionProducts": "Tienda 2",
+        "autoBarcode": "59",
+        "salPriceListId": 3356
+      },
+      "codeTaxes": "01",
+      "discountPercentage": 0,
+      "flagFree": 0,
+      "warehouseName": "TIENDA 1",
+      "brandId": 52669,
+      "quantity": 1,
+      "price": 10,
+      "productType": 1,
+      "salePrice": 10,
+      "stockQuantity": 102,
+      "subtotalWithoutTax": 8.47,
+      "tax": 18,
+      "taxAmount": 1.5254,
+      "priceCost": 0.8475,
+      "unitConversion": 1,
+      "unitId": 11264,
+      "unitPrice": 8.4746,
+      "unitQuantity": 1,
+      "warProductsId": 4125536,
+      "warWarehousesId": 6194,
+      "taxes": [
+        {
+          "amount": 1.5254,
+          "code": "01",
+          "codePercentage": "01",
+          "percentage": 18,
+          "subtotal": 10,
+          "taxSchemeName": "IGV IMPUESTO GENERAL A LAS VENTAS"
+        }
+      ]
+    }
+  ],
+  "currency": "PEN",
+  "commentary": "",
+  "amountCredit": 13,
+  "amountCash": 14,
+  "amount": 14,
+  "tip": 0,
+  "dateOnline": "1710961615674",
+  "warehouseCodeTaxes": null,
+  "warehouseName": "TIENDA 1"
+}
