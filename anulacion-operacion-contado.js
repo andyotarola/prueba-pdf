081422f81a08f7a6444a8df22d514c50855b3726
AndyOtarola <otarolaandy@gmail.com>
2024-03-20 09:33:04 -0500

    Update anulacion-operacion-contado.js

--- anulacion-operacion-contado.js
+++ anulacion-operacion-contado.js
@@ -1,1 +1,145 @@
-
+{
+  "typeCatalogSunatId": 1,
+  "transactionId": [
+    1
+  ],
+  "transactions": [
+    {
+      "amount": -36,
+      "currency": "PEN",
+      "paymentAmount": -36,
+      "paymentDate": "2024-03-20",
+      "typePaymentId": 1
+    }
+  ],
+  "totalTaxesAmount": {
+    "subtotalWithoutTax": 30.51,
+    "iva": 0,
+    "ice": 0,
+    "irbpnr": 0,
+    "tip": 0
+  },
+  "detailsPayments": [],
+  "taxesAmount": {
+    "recorded": 30.51,
+    "inactive": 0,
+    "exonerated": 0,
+    "free": 0,
+    "export": 0,
+    "icbper": 0
+  },
+  "taxes": 5.49,
+  "subtotal": 30.51,
+  "salDocumentsId": 14864492,
+  "paymentMethodId": 1,
+  "flagTransfer": true,
+  "exchangeRate": 3.6,
+  "discount": 0,
+  "details": [
+    {
+      "discount": 0,
+      "brandSpId": 47036,
+      "brandName": "PURA VIDA",
+      "groupType": 3,
+      "categoryName": "AGUA Y HIELO",
+      "categoryId": 55594,
+      "description": "Agua pura vida sin gas 500 ml",
+      "productCode": "7751271033619",
+      "productCubsoCode": null,
+      "unitCode": "SIN",
+      "unitName": "SIN_UNIDAD",
+      "additionalInformation": {
+        "descriptionProducts": "Agua pura vida sin gas 500 ml",
+        "autoBarcode": "24",
+        "salPriceListId": 3356
+      },
+      "codeTaxes": "01",
+      "discountPercentage": 0,
+      "flagFree": 0,
+      "warehouseName": "TIENDA 1",
+      "brandId": 47036,
+      "quantity": 1,
+      "price": 2,
+      "productType": 1,
+      "salePrice": 2,
+      "stockQuantity": 276,
+      "subtotalWithoutTax": 1.69,
+      "tax": 18,
+      "taxAmount": 0.3051,
+      "priceCost": 1.0167,
+      "unitConversion": 1,
+      "unitId": 12282,
+      "unitPrice": 1.6949,
+      "unitQuantity": 1,
+      "warProductsId": 3862975,
+      "warWarehousesId": 6194,
+      "taxes": [
+        {
+          "amount": 0.3051,
+          "code": "01",
+          "codePercentage": "01",
+          "percentage": 18,
+          "subtotal": 2,
+          "taxSchemeName": "IGV IMPUESTO GENERAL A LAS VENTAS"
+        }
+      ]
+    },
+    {
+      "discount": 0,
+      "brandSpId": 38855,
+      "brandName": "S/M",
+      "groupType": 3,
+      "categoryName": "ACEITE",
+      "categoryId": 71772,
+      "description": "Rtretwert",
+      "productCode": "123456789",
+      "productCubsoCode": null,
+      "unitCode": "BAL",
+      "unitName": "BALDE",
+      "additionalInformation": {
+        "descriptionProducts": "Rtretwert",
+        "autoBarcode": "31",
+        "salPriceListId": 3356
+      },
+      "codeTaxes": "01",
+      "discountPercentage": 0,
+      "flagFree": 0,
+      "warehouseName": "TIENDA 1",
+      "brandId": 38855,
+      "quantity": 1,
+      "price": 34,
+      "productType": 1,
+      "salePrice": 34,
+      "stockQuantity": 105,
+      "subtotalWithoutTax": 28.81,
+      "tax": 18,
+      "taxAmount": 5.1864,
+      "priceCost": 0.339,
+      "unitConversion": 1,
+      "unitId": 13858,
+      "unitPrice": 28.8136,
+      "unitQuantity": 1,
+      "warProductsId": 3925886,
+      "warWarehousesId": 6194,
+      "taxes": [
+        {
+          "amount": 5.1864,
+          "code": "01",
+          "codePercentage": "01",
+          "percentage": 18,
+          "subtotal": 34,
+          "taxSchemeName": "IGV IMPUESTO GENERAL A LAS VENTAS"
+        }
+      ]
+    }
+  ],
+  "currency": "PEN",
+  "commentary": "",
+  "amountCredit": 0,
+  "amountCash": 36,
+  "amount": 36,
+  "tip": 0,
+  "dateOnline": "1710945084857",
+  "warehouseCodeTaxes": null,
+  "warehouseName": "TIENDA 1"
+}
